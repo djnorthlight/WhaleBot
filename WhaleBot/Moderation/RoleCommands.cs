40af332c371630e54751d116e037fccdd4e79e4f
Cygaan <30848157+Cygaan@users.noreply.github.com>
2017-09-26 20:28:25 +0200

    Added role command

---
+++ WhaleBot/Moderation/RoleCommands.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using System.IO;
+using System.Diagnostics;
+
+namespace WhaleBot
+{
+    public class RoleCommands : ModuleBase<SocketCommandContext>
+    {
+        [Command("role")]
+        [RequireUserPermission]
+        [RequireBotPermission(GuildPermission.ManageRoles)]
+        [Summary("Adds/removes roles\n**Syntax**: `role User Role(s)`")]
+        public async Task RoleCommand(SocketGuildUser user, params SocketRole[] roles)
+        {
+            var addRoles = new List<SocketRole>();
+            var removeRoles = new List<SocketRole>();
+
+            foreach(SocketRole role in roles)
+            {
+                if (user.Roles.Contains(role)) removeRoles.Add(role);
+                else addRoles.Add(role);
+            }
+
+            await user.AddRolesAsync(addRoles, new RequestOptions { AuditLogReason = $"Added by {Context.User.ToString()}" });
+            await user.RemoveRolesAsync(removeRoles, new RequestOptions { AuditLogReason = $"Removed by {Context.User.ToString()}" });
+
+            List<string> addedRoles = new List<string>();
+            List<string> removedRoles = new List<string>();
+
+            foreach (SocketRole role in addRoles) addedRoles.Add(role.Name);
+            foreach (SocketRole role in removeRoles) removedRoles.Add(role.Name);
+
+            var added = addRoles.Count > 0 ? "Added: " : "";
+            var space = addRoles.Count > 0 && removeRoles.Count > 0 ? " " : "";
+            var removed = removeRoles.Count > 0 ? "Removed: " : "";
+
+            var reply = await ReplyAsync($"Done. {added}{string.Join(", ", addedRoles)}{space}{removed}{string.Join(", ", removedRoles)}");
+
+            var t = Task.Run(async () =>
+            {
+                await Task.Delay(5000);
+                await reply.DeleteAsync();
+                await Context.Message.DeleteAsync();
+            });
+        }
+    }
+}
