d3cec78bb94e58a042437fbc88f4411280ae332f
Cygaan <30848157+Cygaan@users.noreply.github.com>
2017-09-28 16:48:33 +0200

    Fixed the help command

--- WhaleBot/Misc/Commands/HelpCommands.cs
+++ WhaleBot/Misc/Commands/HelpCommands.cs
@@ -22,20 +22,27 @@ namespace WhaleBot
         [Command("help")][Summary("Displays this message")]
         public async Task HelpCommand()
         {
-           var embed =  new EmbedBuilder
-           {
-               Author = new EmbedAuthorBuilder { Name = Context.User.Username, IconUrl = Context.User.GetAvatarUrl() },
-               Title = "Help",
-               Color = new Color(178, 224, 40),
-           }.WithUrl("http://heeeeeeeey.com/");
+            var pages = new List<EmbedBuilder>();
+
+            int currentPage = -1;
 
             foreach(var Command in CommandService.Commands)
             {
-                if(!Command.Preconditions.Contains(new RequireOwnerAttribute()) && !Command.Preconditions.Contains(new WhizlSpecificAttribute())&& Command.Remarks != "Exclude from help") embed.AddField(new EmbedFieldBuilder { Name = $"{string.Join(", ", Command.Aliases)}", Value = Command.Summary ?? "Cygan you cunt theres no summary here"});                
+                if (!Command.Preconditions.Contains(new RequireOwnerAttribute()) && !Command.Preconditions.Contains(new WhizlSpecificAttribute()) && Command.Remarks != "Exclude from help") pages[currentPage].AddField(new EmbedFieldBuilder { Name = $"{string.Join(", ", Command.Aliases)}", Value = Command.Summary ?? "Cygan you cunt theres no summary here" });                
+                if (currentPage == -1 || pages[currentPage].Fields.Count() >= 25)
+                {
+                    currentPage++;
+                    pages.Add(new EmbedBuilder
+                    {
+                        Author = new EmbedAuthorBuilder { Name = Context.User.Username, IconUrl = Context.User.GetAvatarUrl() },
+                        Title = $"Help {currentPage + 1}",
+                        Color = new Color(178, 224, 40),
+                    }.WithUrl("http://heeeeeeeey.com/"));
+                }
             }
 
+            foreach(var page in pages) await Context.User.SendMessageAsync("", false, page);
 
-            await Context.User.SendMessageAsync("", false, embed);
         }
     }
 }
