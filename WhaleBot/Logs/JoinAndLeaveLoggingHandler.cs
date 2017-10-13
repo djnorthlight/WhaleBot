b60c8e4939d079c7a49ad71f4f32067332734d53
Cygaan <30848157+Cygaan@users.noreply.github.com>
2017-10-13 17:23:00 +0200

    Made join/leave embeds prettier

--- WhaleBot/Logs/JoinAndLeaveLoggingHandler.cs
+++ WhaleBot/Logs/JoinAndLeaveLoggingHandler.cs
@@ -29,9 +29,10 @@ namespace WhaleBot
 
             await arg.Guild.GetTextChannel(setup.LeaveChannelId).SendMessageAsync("", false, new EmbedBuilder
             {
-                Title = "User left!",
-                Description = $"User {arg.Mention} ({arg.ToString()}) left",
+                Title = "User left",
+                Description = $"{arg.Mention} (**{arg.ToString()}**) has left the server",
                 Color = new Color(178, 224, 40),
+                Timestamp = DateTime.Now
             }.WithUrl("http://heeeeeeeey.com/"));
         }
 
@@ -43,9 +44,10 @@ namespace WhaleBot
 
             await arg.Guild.GetTextChannel(setup.JoinChannelId).SendMessageAsync("", false, new EmbedBuilder
             {
-                Title = "User joined!",
-                Description = $"User {arg.Mention} ({arg.ToString()}) joined",
+                Title = "User joined",
+                Description = $"{arg.Mention} (**{arg.ToString()}**) has just joined the server!",
                 Color = new Color(178, 224, 40),
+                Timestamp = DateTime.Now,
             }.WithUrl("http://heeeeeeeey.com/"));
         }
 
