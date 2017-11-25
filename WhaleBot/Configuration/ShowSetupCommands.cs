a55a718e5313c604f8b6a4c30981e4ed567d11ea
Cygaan <30848157+Cygaan@users.noreply.github.com>
2017-11-25 19:21:53 +0100

    Fixed setup command so it doesnt break when a channel/role is deleted

--- WhaleBot/Configuration/ShowSetupCommands.cs
+++ WhaleBot/Configuration/ShowSetupCommands.cs
@@ -32,12 +32,12 @@ namespace WhaleBot
                 Color = new Color(178, 224, 40),
                 Fields = new List<EmbedFieldBuilder>
                 {
-                    new EmbedFieldBuilder { Name = "edit_log", Value = setup.EditChannelId == 0 ? "Not set" : Context.Guild.GetTextChannel(setup.EditChannelId).Mention, IsInline = true  },
-                    new EmbedFieldBuilder { Name = "delete_log", Value = setup.RemoveChannelId == 0 ? "Not set" : Context.Guild.GetTextChannel(setup.RemoveChannelId).Mention, IsInline = true  },
-                    new EmbedFieldBuilder { Name = "join_log", Value = setup.JoinChannelId == 0 ? "Not set" : Context.Guild.GetTextChannel(setup.JoinChannelId).Mention, IsInline = true  },
-                    new EmbedFieldBuilder { Name = "leave_log", Value = setup.LeaveChannelId == 0 ? "Not set" : Context.Guild.GetTextChannel(setup.LeaveChannelId).Mention, IsInline = true  },
-                    new EmbedFieldBuilder { Name = "mod_log", Value = setup.ModChannelId == 0 ? "Not set" : Context.Guild.GetTextChannel(setup.ModChannelId).Mention, IsInline = true },
-                    new EmbedFieldBuilder { Name = "muted_role", Value = setup.MutedRoleId == 0 ? "Not set" : Context.Guild.GetRole(setup.MutedRoleId).Mention, IsInline = true },
+                    new EmbedFieldBuilder { Name = "edit_log", Value = setup.EditChannelId == 0 ? "Not set" : Context.Guild.GetTextChannel(setup.EditChannelId)?.Mention, IsInline = true  },
+                    new EmbedFieldBuilder { Name = "delete_log", Value = setup.RemoveChannelId == 0 ? "Not set" : Context.Guild.GetTextChannel(setup.RemoveChannelId)?.Mention, IsInline = true  },
+                    new EmbedFieldBuilder { Name = "join_log", Value = setup.JoinChannelId == 0 ? "Not set" : Context.Guild.GetTextChannel(setup.JoinChannelId)?.Mention, IsInline = true  },
+                    new EmbedFieldBuilder { Name = "leave_log", Value = setup.LeaveChannelId == 0 ? "Not set" : Context.Guild.GetTextChannel(setup.LeaveChannelId)?.Mention, IsInline = true  },
+                    new EmbedFieldBuilder { Name = "mod_log", Value = setup.ModChannelId == 0 ? "Not set" : Context.Guild.GetTextChannel(setup.ModChannelId)?.Mention, IsInline = true },
+                    new EmbedFieldBuilder { Name = "muted_role", Value = setup.MutedRoleId == 0 ? "Not set" : Context.Guild.GetRole(setup.MutedRoleId)?.Mention, IsInline = true },
                 }
             };
 
